6d84aa2e5823521db2ae5b741420790c9c32ceaa
Fagner Silva <fagnerluis@gmail.com>
2017-09-02 22:32:26 -0300

    deleting data

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function getTotal(list) {
 function setList(list) {
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for (var key in list) {
-        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> | Delete</td></tr>'
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> | <button class="btn btn-default" onclick="deleteData('+key+');">Delete</button></td></tr>'
     }
     table += '<tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -75,4 +75,19 @@ function updateData() {
     setList(list);
 }
 
+function deleteData(id) {
+    if(confirm("Delete this item?")) {
+        if(id === list.length -1) {
+            list.pop();
+        }else if(id === 0) {
+            list.shift();
+        }else {
+            var ArrAuxIni = list.slice(0,id);
+            var ArrAuxEnd = list.slice(id + 1);
+            list = ArrAuxIni.concat(ArrAuxEnd);
+        }
+        setList(list);
+    }
+}
+ 
 setList(list);
